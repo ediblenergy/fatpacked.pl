c7764a8afcac77c85915ad7d158c82a4774a42db
skaufman <sam@socialflow.com>
2013-02-17 21:15:30 +0000

    mov error docs to dist dir

--- script/fatpacked.pl
+++ script/fatpacked.pl
@@ -3,8 +3,11 @@ use strictures 1;
 use Plack::Runner;
 use Plack::Builder;
 use FindBin;
+use File::ShareDir qw[ dist_dir ];
 use lib "$FindBin::Bin/../lib";
 use WWW::FatPacked;
+
+my $dist_dir = dist_dir("WWW-FatPacked");
 
 my $runner = Plack::Runner->new;
 $runner->parse_options(@ARGV);
@@ -12,8 +15,8 @@ $runner->run(
     builder {
         enable(
             "Plack::Middleware::ErrorDocument",
-            500 => "$FindBin::Bin/../error_doc/500.html",
-            404 => "$FindBin::Bin/../error_doc/404.html"
+            500 => "$dist_dir/error_doc/500.html",
+            404 => "$dist_dir/error_doc/404.html"
         );
         WWW::FatPacked->psgi_app(@_);
     } );
